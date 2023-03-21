3652463956a8ee8d0a6cf2522f00e1428cd42a91
Caio Francelino <caio.francelinosena@gmail.com>
2023-03-21 18:23:00 -0400

    feat: add find supplier

--- app/controllers/api/v1/suppliers_controller.rb
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::SuppliersController < ApplicationController
   include Paginable
 
+  before_action :find_supplier, only: [:show]
+  rescue_from ActiveRecord::RecordNotFound, with: :supplier_not_found
+
   def index
     @suppliers = Supplier.page(current_page()).per(per_page())
 
@@ -17,9 +20,26 @@ class Api::V1::SuppliersController < ApplicationController
     end
   end
 
+  def show
+    render json: @supplier
+  end
+
   private
+
+  def find_supplier
+    id = params[:id]
+
+    @supplier = Supplier.find(id)
+  end
 
   def supplier_params
     params.require(:supplier).permit(:address, :city, :name, :postal_code, :state)
+  end
+
+  def supplier_not_found
+    render json: {
+      message: "Supplier with id #{params[:id]} does not exists",
+      status: 404
+    }, status: :not_found
   end
 end
