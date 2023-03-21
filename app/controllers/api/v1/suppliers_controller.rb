bc849232f2a7247d41582316203c64eba0789480
Caio Francelino <caio.francelinosena@gmail.com>
2023-03-21 18:13:19 -0400

    feat: add create new supplier

--- app/controllers/api/v1/suppliers_controller.rb
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -6,4 +6,20 @@ class Api::V1::SuppliersController < ApplicationController
 
     render json: @suppliers, meta: meta_attributes(@suppliers), adapter: :json
   end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+
+    if @supplier&.save
+      render json: @supplier, status: :created
+    else
+      render json: @supplier.errors.full_messages, status: :bad_request
+    end
+  end
+
+  private
+
+  def supplier_params
+    params.require(:supplier).permit(:address, :city, :name, :postal_code, :state)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'suppliers/index'
+      resources :suppliers
     end
   end
   root 'home#index'
