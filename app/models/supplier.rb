e88585123abc5e55174bf945962424fc01ed0b41
Caio Francelino <caio.francelinosena@gmail.com>
2023-03-28 22:41:41 -0400

    refactor: add relationships for user and update controllers

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -10,9 +10,19 @@
 #  state       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_suppliers_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class Supplier < ApplicationRecord
   has_many :products
+  belongs_to :user
 
   validates :address, :city, :name, :postal_code, :state, presence: true
 end
--- app/serializers/supplier_serializer.rb
+++ app/serializers/supplier_serializer.rb
@@ -10,6 +10,15 @@
 #  state       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_suppliers_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class SupplierSerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :postal_code, :state, :city
