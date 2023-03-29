6651e0648571cf22825425d09301edaaa0f50b9b
Caio Francelino <caio.francelinosena@gmail.com>
2023-03-28 21:43:57 -0400

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+100.times do
+  Supplier.create!(
+    address: Faker::Address.full_address,
+    city: Faker::Address.city,
+    name: Faker::Name.name,
+    postal_code: Faker::Address.postcode,
+    state: Faker::Address.state
+  )
+end
+
+suppliers = Supplier.all
+
+500.times do
+  Product.create!(
+    amount: Faker::Number.number(digits: 3),
+    min_amount: Faker::Number.number(digits: 2),
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph,
+    unit_price: Faker::Commerce.price(range: 0..10.0),
+    supplier: suppliers.sample,
+    last_entry: Faker::Date.between(from: '2010-01-01', to: '2023-12-31'), 
+    last_outing: Faker::Date.between(from: '2010-01-01', to: '2023-12-31') 
+  )
+end
